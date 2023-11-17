babd06ad19f79f9d8c641f1198c296f3a3ec958d
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-11-17 18:27:25 +0300

    table for position list

--- app/position/page.js
+++ app/position/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from 'next/link';
+import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -10,6 +10,15 @@ import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
 import Chip from "@mui/material/Chip";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 import { useAuthenticatedApi } from "@/hooks/useAuthenticatedApi";
 import { useSession } from "next-auth/react";
 
@@ -38,31 +47,38 @@ export default function Positions() {
   const handleFetchPositions = async () => {
     const params = new URLSearchParams();
 
-    if (filters.settlement_currency.length > 0) params.append('settlement_currency', filters.settlement_currency);
-    if (filters.ticker) params.append('ticker', filters.ticker);
-    if (filters.isin) params.append('isin', filters.isin);
-    filters.currency_influence.forEach(t => params.append('currency_influence', t));
-    filters.type_asset.forEach(t => params.append('type_asset', t));
-    filters.account.forEach(a => params.append('account', a));
-  
-    const fetchedPositions = await api.get(`/api/v1/position/?${params.toString()}`);
+    if (filters.settlement_currency.length > 0)
+      params.append("settlement_currency", filters.settlement_currency);
+    if (filters.ticker) params.append("ticker", filters.ticker);
+    if (filters.isin) params.append("isin", filters.isin);
+    filters.currency_influence.forEach((t) =>
+      params.append("currency_influence", t)
+    );
+    filters.type_asset.forEach((t) => params.append("type_asset", t));
+    filters.account.forEach((a) => params.append("account", a));
+
+    const fetchedPositions = await api.get(
+      `/api/v1/position/?${params.toString()}`
+    );
     setPositions(fetchedPositions.data);
   };
 
   const accounts = ["1", "2", "3", "4", "5", "6"];
   const settlementCurrencies = ["USD", "EUR", "RUB", "BTC"];
-  const currencyInfluence = [['USD', '2'],['EUR', '3'],['RUB', '1']];
-  console.log(currencyInfluence)
+  const currencyInfluence = [
+    ["USD", "2"],
+    ["EUR", "3"],
+    ["RUB", "1"],
+  ];
+  console.log(currencyInfluence);
 
   useEffect(() => {
     console.log(status);
     if (status !== "loading") {
       const getPositionAssetTypes = async () => {
-        const types = await api.get(
-          "/api/v1/position/unique_asset_types/"
-        );
-        console.log(types.data)
-        console.log(Object.entries(types.data))
+        const types = await api.get("/api/v1/position/unique_asset_types/");
+        console.log(types.data);
+        console.log(Object.entries(types.data));
         setAssetTypes(Object.entries(types.data));
       };
 
@@ -81,7 +97,9 @@ export default function Positions() {
         autoComplete="off"
       >
         <FormControl sx={{ m: 1, width: "25ch" }}>
-          <InputLabel id="settlement-currency-label">Settlement currency</InputLabel>
+          <InputLabel id="settlement-currency-label">
+            Settlement currency
+          </InputLabel>
           <Select
             labelId="settlement-currency-label"
             value={filters.settlement_currency}
@@ -117,7 +135,9 @@ export default function Positions() {
           onChange={handleFilterChange}
         />
         <FormControl sx={{ m: 1, width: "25ch" }}>
-          <InputLabel id="currency-influence-label">Currency influence</InputLabel>
+          <InputLabel id="currency-influence-label">
+            Currency influence
+          </InputLabel>
           <Select
             labelId="currency-influence-label"
             id="currency-influence"
@@ -132,8 +152,9 @@ export default function Positions() {
                   <Chip
                     key={value}
                     label={
-                      currencyInfluence.find((currency) => currency[1] === value)?.[0] ||
-                      value
+                      currencyInfluence.find(
+                        (currency) => currency[1] === value
+                      )?.[0] || value
                     }
                   />
                 ))}
@@ -163,8 +184,7 @@ export default function Positions() {
                   <Chip
                     key={value}
                     label={
-                      assetTypes.find((type) => type[1] === value)?.[0] ||
-                      value
+                      assetTypes.find((type) => type[1] === value)?.[0] || value
                     }
                   />
                 ))}
@@ -203,16 +223,50 @@ export default function Positions() {
           </Select>
         </FormControl>
       </Box>
-        <Button variant="contained" onClick={handleFetchPositions}>
-          Apply
-        </Button>
-      <ul>
-         {positions.map(position => (
-        <li key={position.id}>
-          <Link href={`/position/${position.id}`}>{position.asset.ticker}</Link>
-        </li>
-      ))}
-    </ul>
+      <Button variant="contained" onClick={handleFetchPositions}>
+        Apply
+      </Button>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell align="right">Ticker</TableCell>
+              <TableCell align="right">Account</TableCell>
+              <TableCell align="right">Quantity</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Currency</TableCell>
+              <TableCell align="right">Total Value</TableCell>
+              <TableCell align="right">Total Value currency</TableCell>
+              <TableCell align="right">ISIN</TableCell>
+              <TableCell align="right">Asset Name</TableCell>
+              <TableCell align="right">Asset Type</TableCell>
+              <TableCell align="right">Currency Influence</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {positions.map((position, index) => (
+              <TableRow
+                key={position.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">{index + 1}</TableCell>
+                <TableCell align="right">{position.asset.ticker}</TableCell>
+                <TableCell align="right">{position.account}</TableCell>
+                <TableCell align="right">{position.quantity_position}</TableCell>
+                <TableCell align="right">{position.asset.price}</TableCell>
+                <TableCell align="right">{position.asset.currency}</TableCell>
+                <TableCell align="right">{position.total_value}</TableCell>
+                <TableCell align="right">{position.total_value_currency}</TableCell>
+                <TableCell align="right">{position.asset.isin}</TableCell>
+                <TableCell align="right">{position.asset.name_asset}</TableCell>
+                <TableCell align="right">{position.asset.type_asset_display}</TableCell>
+                <TableCell align="right">{position.asset.currency_influence}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
